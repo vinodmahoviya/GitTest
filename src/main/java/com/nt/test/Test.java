5c1a4d32973ff830cd823c3a17c3d8f9059433b6
vinodmahoviya <vinodmahoviya@gmail.com>
2020-04-25 10:31:50 +0530

    addedif condition to check the data

--- src/main/java/com/nt/test/Test.java
+++ src/main/java/com/nt/test/Test.java
@@ -5,6 +5,10 @@ public class Test {
 	
 	public void addData(String data) {
 		System.out.println("data ::"+data);
+		
+		if(data.equals("java techie")){
+			System.out.println("Welcome");
+		}
 	}
 	public static void main(String[] args) {
 		System.out.println("Hello Friend....!");
