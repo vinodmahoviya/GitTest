529d2c9720dd8d58d6fd6d06db7e47e8801c6a45
vinodmahoviya <vinodmahoviya@gmail.com>
2020-04-25 10:27:13 +0530

    added git-test project to local repo

---
+++ src/main/java/com/nt/test/Test.java
@@ -0,0 +1,14 @@
+package com.nt.test;
+
+public class Test {
+
+	
+	public void addData(String data) {
+		System.out.println("data ::"+data);
+	}
+	public static void main(String[] args) {
+		System.out.println("Hello Friend....!");
+
+	}
+
+}
